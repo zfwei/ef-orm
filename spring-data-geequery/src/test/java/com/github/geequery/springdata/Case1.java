764211a625d94a9de5ed1966d0c6d4d3aff251bc
季怡 <jiyi@hikvision.com.cn>
2016-07-29 10:16:57 +0800

    增加测试案例，补充了关于@Param来控制参数名称顺序的支持

--- spring-data-geequery/src/test/java/com/github/geequery/springdata/Case1.java
+++ spring-data-geequery/src/test/java/com/github/geequery/springdata/Case1.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.easyframe.enterprise.spring.CommonDao;
 import org.junit.Test;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -17,6 +18,7 @@ import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
 import com.github.geequery.springdata.test.entity.Foo;
 import com.github.geequery.springdata.test.repo.FooDao;
+import com.github.geequery.springdata.test.repo.FooDao2;
 
 /**
  * 与Spring集成的示例。 本示例使用的xml作为Spring配置。参见
@@ -26,13 +28,16 @@ import com.github.geequery.springdata.test.repo.FooDao;
  *
  */
 @ContextConfiguration(locations = { "classpath:spring-test-data.xml" })
-public class Case1 extends AbstractJUnit4SpringContextTests {
+public class Case1 extends AbstractJUnit4SpringContextTests implements InitializingBean {
 
 	@javax.annotation.Resource
 	private CommonDao commonDao;
 
 	@javax.annotation.Resource
 	private FooDao foodao;
+	
+	@javax.annotation.Resource
+	private FooDao2 foodao2;
 
 	// @Test
 	// public void test1() throws SQLException{
@@ -59,30 +64,11 @@ public class Case1 extends AbstractJUnit4SpringContextTests {
 	// commonDao.removeByField(Foo.class, "id", 1);
 	// }
 	// }
+	
+	
 
 	@Test
 	public void test2() throws SQLException {
-		 commonDao.getNoTransactionSession().createTable(Foo.class);
-		// =========================
-		{
-			List<Foo> list = new ArrayList<Foo>();
-			list.add(new Foo("张三"));
-			list.add(new Foo("李四"));
-			list.add(new Foo("王五"));
-			list.add(new Foo("赵柳"));
-			list.add(new Foo("开发"));
-			list.add(new Foo("测试"));
-			list.add(new Foo("纠结"));
-			list.add(new Foo("刘备"));
-			list.add(new Foo("市场"));
-			list.add(new Foo("渠道"));
-			list.add(new Foo("销售"));
-			list.add(new Foo("赵日天"));
-			list.add(new Foo("叶良辰"));
-			list.add(new Foo("玛丽苏"));
-			list.add(new Foo("龙傲天"));
-			foodao.save(list);	
-		}
 		
 		// =========================
 		{
@@ -126,28 +112,51 @@ public class Case1 extends AbstractJUnit4SpringContextTests {
 			System.out.println("list=" + foos);
 		}
 		
-		// =========================
 		{
-			System.out.println("=== getCountByAge ===");
+			
 		}
 		
-		// =========================
 		{
 			System.out.println("=== updateAgeByName ===");
 		}
 		
-		// =========================
 		{
 			System.out.println("=== findByusername (NativeQuery) ===");
 		}
-		// =========================
 		{
 			System.out.println("=== findBysName (NativeQuery) ===");
 		}
-		// =========================
+		{
+			System.out.println("=== DeleteAll() ===");
+			foodao.deleteAll();	
+		}
+		//测试复合主键的情况
+	}
+	
+	
+	@Test
+	public void testFooDao2(){
+		{
+			System.out.println("=== findByNameLike ===");
+			List<Foo> foo=foodao2.findByNameLike("%四");
+			System.out.println(foo);			
+		}
+
+		{
+			System.out.println("=== findByNameContainsAndAge ===");
+			List<Foo> foos=foodao2.findByNameContainsAndAge("李", 0);
+			System.out.println(foos);
+		}
+		
 		{
 			//多参数顺序测试否有问题
+			System.out.println("=== findByNameStartsWithAndAge ===");
+			List<Foo> foos=foodao2.findByNameStartsWithAndAge(0,"李");
+			System.out.println(foos);
+		}
+		{
 			
+			//====使用GQ NamedQueries
 			
 		}
 		// =========================
@@ -160,10 +169,33 @@ public class Case1 extends AbstractJUnit4SpringContextTests {
 			//NativeQuery多参数是否有问题
 		}
 		
-		// =========================
+		
+		
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		commonDao.getNoTransactionSession().dropTable(Foo.class);
+		commonDao.getNoTransactionSession().createTable(Foo.class);
+		
 		{
-			System.out.println("=== DeleteAll() ===");
-			foodao.deleteAll();	
+			List<Foo> list = new ArrayList<Foo>();
+			list.add(new Foo("张三"));
+			list.add(new Foo("李四"));
+			list.add(new Foo("王五"));
+			list.add(new Foo("赵柳"));
+			list.add(new Foo("开发"));
+			list.add(new Foo("测试"));
+			list.add(new Foo("纠结"));
+			list.add(new Foo("刘备"));
+			list.add(new Foo("市场"));
+			list.add(new Foo("渠道"));
+			list.add(new Foo("销售"));
+			list.add(new Foo("赵日天"));
+			list.add(new Foo("叶良辰"));
+			list.add(new Foo("玛丽苏"));
+			list.add(new Foo("龙傲天"));
+			foodao.save(list);	
 		}
 	}
 }
